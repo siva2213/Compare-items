db6c9976f5c6498dee14c7e5f36ddda3d9853c88
siva1005 <siva.krishna@impactanalytics.co>
2020-04-27 23:03:02 +0530

    test1

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import CompareItem from "./views/CompareItems";
 class App extends Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      name: "siva"
+    };
   }
   render() {
     return (
